58c188f318af0d1feecea5ba6adcef7fd7b1c128
brunocalvi <calvireis@gmail.com>
2023-09-05 22:15:38 -0300

    criando formulario da pergunta

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // MOSTRANDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// ROTAS
 app.get("/", (req, res)=>{
-  res.render("index");
+  res.render("index.ejs");
 });
 
 app.get("/perguntar", (req, res)=>{
-  res.render("perguntar");
+  res.render("perguntar.ejs");
+});
+
+app.post("/salvarpergunta", (req, res)=>{
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+
+  res.send(`formulário recebido! | titulo: ${titulo} descricao: ${descricao}`);
 });
 
 app.listen(8000, ()=>{
